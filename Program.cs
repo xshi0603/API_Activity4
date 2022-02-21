fa0d14a26c8bc9b298b0185caa6958f5f42cdcd6
xshi0603 <xshi603@gmail.com>
2022-02-20 22:46:45 -0500

    done

--- Program.cs
+++ Program.cs
@@ -1,67 +1,64 @@
 using System;
 
-public delegate void ExampleDelegate();
-
-interface TesterInterface {
+interface TesterInterface
+{
 	//instance methods
 	void testMethod();
 
 	//property
-	string Item { get; set; }
+	string testString { get; set; }
 
 	//indexer
-	string this[int i] {
+	string this[int i]
+	{
 		get;
 		set;
 	}
 }
 
-class TesterClass : TesterInterface {
+class TesterClass : TesterInterface
+{
 	//instance method
-	void testMethod() {
+	public void testMethod()
+	{
 		Console.WriteLine("Hello!");
 	}
 
 	//property
 	private string name = "Test Name";
-	public string Item {
-		get {
-			return name;
-		}
-		set {
-			name = value;
-		}
+
+	public string testString
+	{
+		get { return name; }
+		set { name = value; }
 	}
 
 	//indexer
 	private string[] arr = new string[100];
-	
+
 	public string this[int i] {
 		get { return arr[i]; }
 		set { arr[i] = value; }
 	}
 }
 
-class Program {
-	static void Main() {
+class Program
+{
+	static void Main()
+	{
 		TesterClass tc1 = new TesterClass();
 
 		//instance method
 		tc1.testMethod();
 
 		//property
-		tc1.Item = "test string";
-		Console.WriteLine(tc1.Item);
+		tc1.testString = "test string";
+		Console.WriteLine(tc1.testString);
 
 		//indexer
-		tc1[5] = 100;
+		tc1[5] = "Hello";
 
 		Console.WriteLine(tc1[4]);
 		Console.WriteLine(tc1[5]);
 	}
 }
-
-/*
- * Has an interface
- * Three different properties: Instance methods, properties, events, indexers
- */
